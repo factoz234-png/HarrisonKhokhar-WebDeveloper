807309747b187fdc4578c592671c5b2175b91c38
Harrison Khokhar <factoz234@gmail.com>
2026-02-20 14:53:12 +0500

    Nav Modified

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ const navMenu = document.getElementById("nav-menu");
 hammerburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+
+window.onscroll = function () {
+  const nav = document.querySelector(".nav");
+  if (window.scrollY > 50) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
+};
